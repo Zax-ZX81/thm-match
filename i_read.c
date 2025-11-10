a677ec600112cb0310bc80d22f5a13d3cc058b74
Zax-ZX81 <zx81@zx81.nz>
2025-11-11 02:05:23 +1300

    Fixed right side of thumbnail, bottom to go.

--- i_read.c
+++ i_read.c
@@ -5,49 +5,54 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-struct rgb_return get_pixel (unsigned char *data, int anch, int width);
+struct rgb_return get_pixel (unsigned char *data, int anch, int width, int y_max, int x_max);
 
 int main (void)
 {
 int width, height, comp;
-int rlp, olp, px = 0;
+int rlp, olp, wid_mod;
 long racc = 0, gacc = 0, bacc = 0;
 unsigned char *data = stbi_load("img/PJH.png", &width, &height, &comp, 0);
 struct rgb_return pix_return;
 
 FILE *out_thumbnail;
 out_thumbnail = fopen ("img/P.rgb", WRITE_BINARY);
+wid_mod = width % 64;
 for (olp = 0; olp < 64; olp++)
 	{
-	for (rlp = 0; rlp < 64; rlp++)
+	for (rlp = 0; rlp < 63; rlp++)
 		{
-		pix_return = get_pixel (data, (olp * (width * 3) * (width / 64)) + (rlp * (width / 64) * 3), width);
+		pix_return = get_pixel (data, (olp * (width * 3) * (width / 64)) + (rlp * (width / 64) * 3), width, width / 64, width / 64);
 		racc = racc + pix_return.red_val;
 		gacc = gacc + pix_return.grn_val;
 		bacc = bacc + pix_return.blu_val;
-		px++;
-		fputc (pix_return.red_val / 324, out_thumbnail);
-		fputc (pix_return.grn_val / 324, out_thumbnail);
-		fputc (pix_return.blu_val / 324, out_thumbnail);
+		fputc (pix_return.red_val / (int) pow (width / 64, 2), out_thumbnail);
+		fputc (pix_return.grn_val / (int) pow (width / 64, 2), out_thumbnail);
+		fputc (pix_return.blu_val / (int) pow (width / 64, 2), out_thumbnail);
 //printf ("O=%5d\tR=%2d, G=%2d, R=%2d\n", olp, pix_return.red_val / 1296, pix_return.grn_val / 1296, pix_return.blu_val / 1296);
-//		printf ("\n");
-//	printf ("R=%d, G=%d, R=%d\tO=%d, A=%d\n", pix_return.red_val / 1296, pix_return.grn_val / 1296, pix_return.blu_val / 1296, olp, anch);
 		}
+	pix_return = get_pixel (data, (olp * (width * 3) * (width / 64)) + (63 * (width / 64) * 3), width, width / 64, width / 64 + wid_mod);
+	racc = racc + pix_return.red_val;
+	gacc = gacc + pix_return.grn_val;
+	bacc = bacc + pix_return.blu_val;
+	fputc (pix_return.red_val / (width / 64 * (width / 64 + wid_mod)), out_thumbnail);
+	fputc (pix_return.grn_val / (width / 64 * (width / 64 + wid_mod)), out_thumbnail);
+	fputc (pix_return.blu_val / (width / 64 * (width / 64 + wid_mod)), out_thumbnail);
 	}
-printf ("R=%2ld, G=%2ld, R=%2ld\n", racc / 5308416, gacc / 5308416, bacc / 5308416);
+printf ("R=%2ld, G=%2ld, R=%2ld\tWM=%d\n", racc, gacc, bacc, wid_mod);
 
 fclose (out_thumbnail);
 return 0;
 }
 
-struct rgb_return get_pixel (unsigned char *data, int anch, int width)
+struct rgb_return get_pixel (unsigned char *data, int anch, int width, int y_max, int x_max)
 {
 int yl, xl, px, rc = 0, gc = 0, bc = 0;
 struct rgb_return pix_return;
 
-for (yl = 0; yl < width / 64; yl++)
+for (yl = 0; yl < y_max; yl++)
 	{
-	for (xl = 0; xl < width / 64; xl++)
+	for (xl = 0; xl < x_max; xl++)
 		{
 		px = anch + (yl * (width * 3)) + (xl * 3);
 		rc = rc + data [px];
