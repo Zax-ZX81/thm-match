eef5287b2d7006aaa89e600e80dfdd7a77347985
Zax-ZX81 <zx81@zx81.nz>
2025-11-17 03:19:48 +1300

    Chipping away at i_read.

--- i_read.c
+++ i_read.c
@@ -10,8 +10,10 @@ struct rgb_return get_pixel (unsigned char *i_buff, int anch, int width, int y_m
 int main (int argc, char *argv [])
 {
 int width, height, comp;
-int rlp, olp, wid_div, wid_mod, pix_ofs = 0, ir_pos = 0, pix_cnt, max_side;
+int rlp, olp, scl_div, scl_mod, pix_cnt, max_side, y_scl_rmd, x_scl_rmd;
+int pix_ofs = 0, ir_pos = 0, y_inc = 0, x_inc = 0;
 long racc = 0, gacc = 0, bacc = 0;
+float scl_div_fp;
 char img_name [FILENAME_LENGTH] = NULL_STRING;
 unsigned char *img_raw, *i_buff;
 char aspect = 0;
@@ -39,9 +41,12 @@ if (width < height)
 	max_side = width;
 	}
 pix_cnt = max_side * max_side;
-wid_div = max_side / 64;
-wid_mod = max_side % 64;
-printf ("W=%d, H=%d, C=%d, A=%d, PC=%d, WD=%d, WM=%d\n", width, height, comp, aspect, pix_cnt, wid_div, wid_mod);
+scl_div = max_side / 64;
+scl_mod = max_side % 64;
+scl_div_fp = (float) max_side / 64;
+y_scl_rmd = scl_mod;
+x_scl_rmd = scl_mod;
+printf ("W=%d, H=%d, C=%d, A=%d, PC=%d, SD=%d, SM=%d, Sf=%5.3f\n", width, height, comp, aspect, pix_cnt, scl_div, scl_mod, scl_div_fp);
 i_buff = (unsigned char *) calloc (pix_cnt, 3);
 if (aspect != SQUARE)
 	{
@@ -83,71 +88,61 @@ switch (aspect)
 	}
 printf ("W=%d, H=%d, C=%d, A=%d\n", width, height, comp, aspect);
 
-/*###############################################*/
-if (!wid_mod)
+if (!scl_mod)
 	{
 	printf ("Clean divide\n");
 	for (olp = 0; olp < 64; olp++)
 		{
 		for (rlp = 0; rlp < 64; rlp++)
 			{
-			pix_return = get_pixel (i_buff, (olp * (max_side * 3) * wid_div) + (rlp * wid_div * 3), max_side, wid_div, wid_div);
+			pix_return = get_pixel (i_buff, (olp * (max_side * 3) * scl_div) + (rlp * scl_div * 3), max_side, scl_div, scl_div);
 			racc = racc + pix_return.red_val;
 			gacc = gacc + pix_return.grn_val;
 			bacc = bacc + pix_return.blu_val;
-			fputc (pix_return.red_val / pow (wid_div, 2), out_thumbnail);
-			fputc (pix_return.grn_val / pow (wid_div, 2), out_thumbnail);
-			fputc (pix_return.blu_val / pow (wid_div, 2), out_thumbnail);
+			fputc (pix_return.red_val / pow (scl_div, 2), out_thumbnail);
+			fputc (pix_return.grn_val / pow (scl_div, 2), out_thumbnail);
+			fputc (pix_return.blu_val / pow (scl_div, 2), out_thumbnail);
 //printf ("OL=%2d\tRL=%2d\tR=%2d, G=%2d, R=%2d\n", olp, rlp, pix_return.red_val / 1296, pix_return.grn_val / 1296, pix_return.blu_val / 1296);
 			}
 		}
 	}
 	else
 	{
-/*###############################################*/
-/*###############################################*/
 	printf ("Remainder\n");
-	for (olp = 0; olp < 63; olp++)
+	for (olp = 0; olp < 64; olp++)
 		{
-		for (rlp = 0; rlp < 63; rlp++)
+		if ((float) (scl_div_fp * olp) > (float) (scl_div * olp + 0.5 + y_inc))
 			{
-			pix_return = get_pixel (i_buff, (olp * (max_side * 3) * wid_div) + (rlp * wid_div * 3), max_side, wid_div, wid_div);
+printf ("y\n");
+			y_inc++;
+			y_scl_rmd--;
+			}
+		for (rlp = 0; rlp < 64; rlp++)
+			{
+			if (((float) scl_div_fp * rlp) > (float) (scl_div * rlp + 0.5 + x_inc))
+				{
+printf ("x\n");
+				x_inc++;
+				x_scl_rmd--;
+				}
+			pix_return = get_pixel (i_buff, (olp * (max_side * 3) * scl_div) + (rlp * scl_div * 3), max_side, scl_div + y_inc, scl_div + x_inc);
+printf ("Of=%5.3f, Oi=%5.3f, Rf=%5.3f, Ri=%5.3f\n", (float) (scl_div_fp * olp), \
+						(float) (scl_div * olp + 0.5 + y_inc), \
+						(float) (scl_div_fp * rlp), \
+						(float) (scl_div * rlp + 0.5 + x_inc));
+printf ("O=%2d, R=%2d, A=%7d, M=%5d, Y=%3d, X=%3d\n", olp, rlp, (olp * (max_side * 3) * scl_div) + (rlp * scl_div * 3), max_side, scl_div + y_inc, scl_div + x_inc);
 			racc = racc + pix_return.red_val;
 			gacc = gacc + pix_return.grn_val;
 			bacc = bacc + pix_return.blu_val;
-			fputc (pix_return.red_val / pow (wid_div, 2), out_thumbnail);
-			fputc (pix_return.grn_val / pow (wid_div, 2), out_thumbnail);
-			fputc (pix_return.blu_val / pow (wid_div, 2), out_thumbnail);
+			fputc (pix_return.red_val / pow (scl_div, 2), out_thumbnail);
+			fputc (pix_return.grn_val / pow (scl_div, 2), out_thumbnail);
+			fputc (pix_return.blu_val / pow (scl_div, 2), out_thumbnail);
 //printf ("O=%5d\tR=%2d, G=%2d, R=%2d\n", olp, pix_return.red_val / 1296, pix_return.grn_val / 1296, pix_return.blu_val / 1296);
 			}
-		pix_return = get_pixel (i_buff, (olp * (max_side * 3) * wid_div) + (63 * wid_div * 3), max_side, wid_div, wid_div + wid_mod);
-		racc = racc + pix_return.red_val;
-		gacc = gacc + pix_return.grn_val;
-		bacc = bacc + pix_return.blu_val;
-		fputc (pix_return.red_val / (wid_div * (wid_div + wid_mod)), out_thumbnail);
-		fputc (pix_return.grn_val / (wid_div * (wid_div + wid_mod)), out_thumbnail);
-		fputc (pix_return.blu_val / (wid_div * (wid_div + wid_mod)), out_thumbnail);
 		}
-	for (rlp = 0; rlp < 63; rlp++)
-		{
-		pix_return = get_pixel (i_buff, (63 * (max_side * 3) * wid_div) + (rlp * wid_div * 3), max_side, wid_div + wid_mod, wid_div);
-		racc = racc + pix_return.red_val;
-		gacc = gacc + pix_return.grn_val;
-		bacc = bacc + pix_return.blu_val;
-		fputc (pix_return.red_val / (wid_div * (wid_div + wid_mod)), out_thumbnail);
-		fputc (pix_return.grn_val / (wid_div * (wid_div + wid_mod)), out_thumbnail);
-		fputc (pix_return.blu_val / (wid_div * (wid_div + wid_mod)), out_thumbnail);
-//printf ("O=%5d\tR=%2d, G=%2d, R=%2d\n", olp, pix_return.red_val / 1296, pix_return.grn_val / 1296, pix_return.blu_val / 1296);
-		}
-	pix_return = get_pixel (i_buff, (63 * (max_side * 3) * wid_div) + (63 * wid_div * 3), max_side, wid_div + wid_mod, wid_div + wid_mod);
-	printf ("R=%d, G=%d, B=%d\n", pix_return.red_val, pix_return.grn_val, pix_return.blu_val);
-	fputc (pix_return.red_val / ((wid_div + wid_mod) * (wid_div + wid_mod)), out_thumbnail);
-	fputc (pix_return.grn_val / ((wid_div + wid_mod) * (wid_div + wid_mod)), out_thumbnail);
-	fputc (pix_return.blu_val / ((wid_div + wid_mod) * (wid_div + wid_mod)), out_thumbnail);
 	}
-/*###############################################*/
 
-printf ("R=%2ld, G=%2ld, R=%2ld\tWM=%d\n", racc, gacc, bacc, wid_mod);
+printf ("R=%2ld, G=%2ld, R=%2ld\tWM=%d\n", racc, gacc, bacc, scl_mod);
 
 fclose (out_thumbnail);
 return 0;
